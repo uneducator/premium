ca1f845eede0bf7d53eff44a74a4c8ab77b40991
wyvernated <wyvernated@gmail.com>
2023-02-27 17:25:27 +0000

    e

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const passwordInput = document.getElementById('password');
+const errorMessage = document.createElement('p');
+errorMessage.classList.add('error-message');
+errorMessage.textContent = 'Incorrect password. Please try again.';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const password = passwordInput.value;
+  if (password === 'DCS_Uneducator') {
+    localStorage.setItem('authenticated', true);
+    window.location.href = '/home';
+  } else {
+    form.appendChild(errorMessage);
+  }
+});
